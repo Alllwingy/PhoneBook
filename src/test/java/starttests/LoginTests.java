885b97caf8de9bdcd37bfd427fcbd1d10641acac
Alllwingy <trots.david@gmail.com>
2023-09-23 22:45:02 +0300

    Hw_07_LoginRegistrationTests

--- src/test/java/starttests/LoginTests.java
+++ src/test/java/starttests/LoginTests.java
@@ -1,2 +1,28 @@
-package starttests;public class LoginTests {
+package starttests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class LoginTests extends BaseTest {
+
+    @Test
+    public void loginTests() throws IOException, InterruptedException {
+
+        driver.findElement(By.xpath(navigationLoginButtonAnd)).click();
+
+        findInputForLogin(byEmailLocator);
+        findInputForLogin(byPasswordLocator);
+
+        driver.findElement(By.xpath(loginButtonAnd)).click();
+
+        WebElement testIfPresent = driver.findElement(By.xpath(navigationContactsButtonActiveTest));
+        String actualText = testIfPresent.getText().trim().toUpperCase();
+        String expectedText = "CONTACTS".toUpperCase();
+
+        Assert.assertEquals(actualText, expectedText);
+    }
 }
